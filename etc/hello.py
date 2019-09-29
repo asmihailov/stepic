8a8cb80b5736b74c4466da0f39e187024fbafe20
student <norett89@gmail.com>
2019-09-29 22:52:12 +0000

    second lesson

---
+++ etc/hello.py
@@ -0,0 +1,12 @@
+def wsgi_app(environ, start_response):
+	body = ''
+	begin = environ['QUERY_STRING'].find('?')
+	for i in environ['QUERY_STRING'][begin:].split('&'):
+		body += str(i) + '/n'
+	status = '200 OK'
+	headers = [('Content-Type','text/plain')]
+	
+	start_response(status, headers)
+
+	return [body]
+
