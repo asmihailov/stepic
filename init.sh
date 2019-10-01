65632795ef43ab8aad4327707b48751e2d80516b
1 <1@1.1>
2019-10-01 22:01:37 +0000

    2.1

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gask.conf /etc/gunicorn.d/test
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/gunicorn restart
 
